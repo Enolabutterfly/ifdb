cc08fe58f7199ecc851e49b47ed8b256d91c8faa
Alexander Lyashuk <mooskagh@gmail.com>
2017-03-05 14:07:56 +0100

    Basic template.

--- games/views.py
+++ games/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return render_to_response('games/index.html')
